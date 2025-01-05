d28feb2603c254e831362badff88bea176f0cad3
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-05 20:52:34 +0500

    New Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import Button from './components/Button/Button'
 
 const App = () => {
   return (
-   <>
-     <Button title={"New Button"}/>
-   </>
+    <div>
+      Hello
+    </div>
   )
 }
 
