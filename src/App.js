2ec851f8074a1c0a6d6cb0d562467979fe5c2388
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-05 20:32:11 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from './components/Button/Button'
+
+const App = () => {
+  return (
+   <>
+     <Button title={"New Button"}/>
+   </>
+  )
+}
+
+export default App
